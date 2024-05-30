d34d5139e00ee9b67cd331db186aca1cdcfba726
rafael_nunes <rafa.nuns17@gmail.com+>
2024-05-30 01:48:19 -0300

    Programando classe de Produto e ProdutoDao

--- src/trocaid/model/dao/ProdutoDao.java
+++ src/trocaid/model/dao/ProdutoDao.java
@@ -1,10 +1,47 @@
 package trocaid.model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import trocaid.model.entities.Produto;
+
 /**
  *
  * @author RafaelNunes
  */
 public class ProdutoDao {
-    
-    
+
+    private Connection connection;
+
+    public ProdutoDao(Connection connection) {
+        this.connection = connection;
+    }
+
+    public List<Produto> getListaDeProdutos(Integer iniciador) throws SQLException {
+        return listProdutos(iniciador);
+    }
+
+    private List<Produto> listProdutos(Integer iniciador) throws SQLException {
+        List<Produto> list = new ArrayList<Produto>();
+        try (PreparedStatement pstm = connection.prepareStatement("SELECT id, codigo FROM produto;");
+                ResultSet rs = pstm.executeQuery()) {
+
+            int n = iniciador;
+            while (rs.next()) {
+                Produto p = new Produto();
+                p.setId(rs.getInt("id"));
+                p.setCodigo(rs.getString("codigo"));
+                p.setIdAntiga(rs.getInt("id"));
+                // Defino ID pela posição do produto na lista
+                p.setIdNova(n);
+                n++;
+                list.add(p);
+                System.out.println(p);
+            }
+        }
+        return list;
+    }
 }
--- src/trocaid/model/entities/Produto.java
+++ src/trocaid/model/entities/Produto.java
@@ -1,9 +1,85 @@
 package trocaid.model.entities;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  *
  * @author RafaelNunes
  */
-public class Produto {
-    
+public class Produto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String codigo;
+    private Integer idAntiga;
+    private Integer idNova;
+
+    public Produto() {
+    }
+
+    public Produto(Integer id, String codigo, Integer idAntiga, Integer idNova) {
+        super();
+        this.id = id;
+        this.idAntiga = idAntiga;
+        this.idNova = idNova;
+        this.codigo = codigo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Integer getIdAntiga() {
+        return idAntiga;
+    }
+
+    public void setIdAntiga(Integer idAntiga) {
+        this.idAntiga = idAntiga;
+    }
+
+    public Integer getIdNova() {
+        return idNova;
+    }
+
+    public void setIdNova(Integer idNova) {
+        this.idNova = idNova;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idAntiga);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Produto other = (Produto) obj;
+        return Objects.equals(idAntiga, other.idAntiga);
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + " - CODIGO: " + codigo + " - ID_ANTIGA: " + idAntiga + " - ID_NOVA_CONTADOR: " + idNova;
+    }
 }
