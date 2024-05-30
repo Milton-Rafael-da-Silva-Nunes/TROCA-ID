795c4cfaa72be6dd9926bb28c9145515cd04952f
rafael_nunes <rafa.nuns17@gmail.com+>
2024-05-30 01:36:18 -0300

    Programando inicialização de tela inicial

--- src/trocaid/Program.java
+++ src/trocaid/Program.java
@@ -1,6 +1,13 @@
 package trocaid;
 
+import java.io.IOException;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -11,7 +18,29 @@ public class Program extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/trocaid/view/ViewFXML.fxml"));
+            Parent parent = loader.load();
+            
+            // Carregar a logo
+            Image logo = new Image(getClass().getResourceAsStream("/trocaid/view/img/logoNova.png"));
+            primaryStage.getIcons().add(logo);
+            
+            // Adicionar a imagem de fundo
+            Image fundo = new Image(getClass().getResourceAsStream("/trocaid/view/img/fundo.png"));
+            ImageView imageView = new ImageView(fundo);
+            imageView.setFitWidth(800);
+            imageView.setFitHeight(500);
+            StackPane stackPane = new StackPane(imageView, parent);
+            
+            Scene mainScene = new Scene(stackPane);
+            primaryStage.setScene(mainScene);
+            primaryStage.setTitle("LC muda ID Produto");
+            primaryStage.show();
+            primaryStage.setResizable(false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
