1b96555660f869a2e7af7ced0cbd63121d4d0d14
rafael_nunes <rafa.nuns17@gmail.com+>
2024-05-30 01:50:34 -0300

    programando classes utilitarias

---
+++ src/trocaid/model/util/Constraints.java
@@ -0,0 +1,26 @@
+package trocaid.model.util;
+
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author RafaelNunes
+ */
+public class Constraints {
+
+    public static void setTextFieldInteger(TextField txt) {
+        txt.textProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue != null && !newValue.matches("\\d*")) {
+                txt.setText(oldValue);
+            }
+        });
+    }
+
+    public static void setTextFieldMaxLength(TextField txt, int max) {
+        txt.textProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue != null && newValue.length() > max) {
+                txt.setText(oldValue);
+            }
+        });
+    }
+}
